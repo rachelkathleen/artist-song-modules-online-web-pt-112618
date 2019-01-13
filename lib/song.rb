4851806d77f295b1e666781d62bc5266c3297e84
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-13 21:06:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,12 +11,8 @@ class Song
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@songs
