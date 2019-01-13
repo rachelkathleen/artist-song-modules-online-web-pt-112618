08a484ba0ee9860d6ccb98344972320678714d24
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-13 21:10:39 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,16 +5,15 @@ class Artist
   attr_reader :songs
 
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -28,9 +27,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -5,16 +5,11 @@ class Song
   attr_reader :artist
 
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
@@ -22,9 +17,5 @@ class Song
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
