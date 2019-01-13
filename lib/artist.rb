aad1fe41b3dbb56fd2ef5623cd5314a24b0a92ca
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-13 21:01:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,6 @@ class Artist
     @@artists << self
     @songs = []
   end
-  #
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@artists
@@ -31,7 +27,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
